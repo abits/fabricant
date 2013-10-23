e0cc5051229ccc261f8b523424a54451abe6381a
Chris Martel <chris@codeways.org>
2013-10-23 23:05:26 +0200

    changed project layout

---
+++ fabricant/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#!/bin/env python2
+
+from fabric.api import *
+from fabric.utils import *
+from fabric.colors import *
+import telnetlib
+import os
+from time import sleep
+
+
